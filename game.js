2beb8edac5f1eddbd31501501e2f4345820e4b2b
Alin Tanislav <alintanislav23@gmail.com>
2026-01-04 13:00:41 +0200

    Added files and made minor changes to js file

--- game.js
+++ game.js
@@ -1,8 +1,9 @@
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
+window.onload = () => {
+const gameCanvas = document.getElementById('gameCanvas');
+const gameContext = gameCanvas.getContext('2d');
 
-const NOTE_SPEED = 3;
-const SPAWN_RATE = 1000;
+const noteSpeed = 3;
+const spawnRate = 1000;
 
 let notes = [];
 
@@ -10,7 +11,7 @@ function spawnNote(){
     const isRed = Math.random() > 0.5;
 
     const note = {
-        x: canvas.width,
+        x: gameCanvas.width,
         y: 80,
         width: 50,
         height: 50,
@@ -21,15 +22,15 @@ function spawnNote(){
 }
 
 function draw(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    gameContext.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
 
     for(let i = 0; i < notes.length; i++){
         const note = notes[i];
 
-        note.x = note.x - NOTE_SPEED;
+        note.x = note.x - noteSpeed;
 
-        ctx.fillStyle = note.color;
-        ctx.fillRect(note.x,note.y,note.width, note.height);
+        gameContext.fillStyle = note.color;
+        gameContext.fillRect(note.x,note.y,note.width, note.height);
 
         if(note.x + note.width < 0){
             notes.splice(i,1);
@@ -40,5 +41,8 @@ function draw(){
     requestAnimationFrame(draw);
 }
 
-setInterval(spawnNote, SPAWN_RATE);
+setInterval(spawnNote, spawnRate);
 draw();
+
+}
+
